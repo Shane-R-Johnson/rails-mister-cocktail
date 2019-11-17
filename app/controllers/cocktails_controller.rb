4655d8966311d2a2acaf2baba8b66cf930987811
Shane Johnson <sj34606@gmail.com>
2019-11-17 23:46:03 +0000

    cocktail WIP

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,9 +1,63 @@
 class CocktailsController < ApplicationController
-  def index
+
+  def index         # GET /restaurants
     @cocktails = Cocktail.all
+  end
+
+  def show          # GET /restaurants/:id
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new           # GET /restaurants/new
+    @cocktail = Cocktail.find(params[new_cocktail])
+  end
+
+  def create        # POST /restaurants
+    @cocktail = Cocktail.new(name: params[:name], address: params[:address])
+    @cocktail.save
+  end
+
+  def edit          # GET /restaurants/:id/edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update        # PATCH /restaurants/:id
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+  end
+
+  # def update
+  #   if @restaurant.update(restaurant_params)
+  #     redirect_to restaurant_path(@restaurant)
+  #   else
+  #     render :edit
+  #   end
+
+  def destroy       # DELETE /restaurants/:id
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+
+# def destroy
+#     @list = List.find(params[:id])
+#     @list.destroy
+#     redirect_to lists_path
+#   end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
 
 
-
-param.require(:cocktail).permit(:name)
+  # def update
+  #   person = current_account.people.find(params[:id])
+  #   person.update!(person_params)
+  #   redirect_to person
+  # end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,23 @@
 class DosesController < ApplicationController
+# app/controller/restaurants_controller.rb
+  def index         # GET /restaurants
+  end
+
+  def show          # GET /restaurants/:id
+  end
+
+  def new           # GET /restaurants/new
+  end
+
+  def create        # POST /restaurants
+  end
+
+  def edit          # GET /restaurants/:id/edit
+  end
+
+  def update        # PATCH /restaurants/:id
+  end
+
+  def destroy       # DELETE /restaurants/:id
+  end
 end
